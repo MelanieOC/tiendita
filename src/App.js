0e825adbea267cb508f78ed03f8db92e9af3cf9a
MelanieOC <melanie.ocharan@gmail.com>
2017-10-26 15:55:31 -0500

    primera vista

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+//import './App.css';
+import './body.css';
 import products from './data';
 //import { Panel, Image, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { connect } from 'redux-zero/react';
@@ -47,14 +48,49 @@ class App extends Component {
 
 const App = ({ allDish }) => {
   return (
-    <Grid>
-      <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-        {
-          allDish.map(item => <Dish image={item.image} name={item.dish} price={item.price} addToCart="" navDetails="#" />)
-        }
+    <div id="application">
+      <div>
+        <div id="wrapper">
+          <div id="header">
+            <h1 className="logo"><a href="#/"></a></h1>
+            <a id="cart-info" href="#">Shopping Cart<span><span data-bind="text: cart.contentsCount"></span> items</span></a>
+          </div>
+          <p data-bind="visible: cart.cleared" >Thank you for your order!</p>
+          <div id="main-section">
+            <section id="pre-content">
+              <div>
+                <div id="shop-info" data-bind="attr:{className: cartContentsClass}" className="empty">
+
+                </div>
+              </div>
+
+            </section>
+            <section id="content">
+              <div>
+                <ul data-role="listview" data-bind="source:items" data-template="item" id="main" className="k-widget k-listview" role="listbox">
+                  {
+                    allDish.map(item => <li><Dish image={item.image} name={item.dish} price={item.price} addToCart="" navDetails="#" /></li>)
+                  }
+                </ul>
+              </div>
+
+            </section>
+
+          </div>
+          <div id="footer">
+            <p>"Learn more about Kendo UI"
+                <sup>®</sup>
+              "SPA at"
+                <a href="http://docs.telerik.com/kendo-ui/getting-started/framework/spa/overview">docs.telerik.com/kendo-ui</a>
+              "."
+                </p>
+
+            <p>Copyright © 2017, Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.</p>
+          </div>
+        </div>
 
       </div>
-    </Grid>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Body from './Body'
 import registerServiceWorker from './registerServiceWorker';
 import store from './Store';
 import {Provider} from 'redux-zero/react';
