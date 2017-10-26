9f9256e8fdcc7d5fae6cc7175bc97eec79ae4342
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-26 15:58:10 -0500

    quitando warper

--- src/CheckoutOrder.js
+++ src/CheckoutOrder.js
@@ -25,35 +25,33 @@ const CheckoutOrder = ({ selected }) => {
         );
     });
     return (
-        <div id="wrapper">
-            <section id='main-section'>
-                <div id='content'>
-                    <div id="checkout-top-image" />
-                    <div id="details-checkout">
-                        <h1>Order Details</h1>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Item</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody className="k-widget k-listview" role="listbox">
-                                {products}
-                            </tbody>
-                        </table>
-                        <p id="total-checkout">
-                            <em>total:</em>
-                            <span>$4.00</span>
-                        </p>
-                        <a className="cancel-order" href="#">cancel order</a>
-                        <button className="order-now">order now!</button>
-                    </div>
-                    <div id="checkout-bottom-image" />
+        <section id='main-section'>
+            <div id='content'>
+                <div id="checkout-top-image" />
+                <div id="details-checkout">
+                    <h1>Order Details</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Item</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody className="k-widget k-listview" role="listbox">
+                            {products}
+                        </tbody>
+                    </table>
+                    <p id="total-checkout">
+                        <em>total:</em>
+                        <span>$4.00</span>
+                    </p>
+                    <a className="cancel-order" href="#">cancel order</a>
+                    <button className="order-now">order now!</button>
                 </div>
-            </section>
-        </div>
+                <div id="checkout-bottom-image" />
+            </div>
+        </section>
     )
 }
 
