989fccfe5177475009642406103669467d27e5cb
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-26 15:45:58 -0500

    finalizado

--- src/body.js
+++ src/body.js
@@ -2,60 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Body.css';
 
-const Body = () => {
+const Body = ()=> {
+  
+     return (
 
-    return (
-        <body>
-            <div id="application">
-                <div>
-                    <div id="wrapper">
-                        <div id="header">
-                            <h1 class="logo">
-                                <a href="#/"></a>
-                            </h1>
-                            <a id="cart-info" href="#">
-                                "Shopping Cart"
-                         <span>
-                                    <span data-bing="text: cart.contentsCount"></span>
-                                </span>
-                            </a>
-                        </div>
-                        <p data-bind="visible: cart.cleared" >Thank you for your order!</p>
-                        <div id="main-section">
-                            <section id="pre-content">
-                                <div>
-                                    <div id="shop-info" data-bind="attr:{class: cartContentsClass}" class="empty">
-
-                                    </div>
-                                </div>
-
-                            </section>
-                            <section id="content">
-                                <div>
-                                    <ul data-role="listview" data-bind="source:items" data-template="item" id="main" class="k-widget k-listview" role="listbox"></ul>
-                                </div>
-
-                            </section>
-
-                        </div>
-                        <div id="footer">
-                            <p>"Learn more about Kendo UI"
-                        <sup>®</sup>
-                                "SPA at"
-                        <a href="http://docs.telerik.com/kendo-ui/getting-started/framework/spa/overview">docs.telerik.com/kendo-ui</a>
-                                "."
-                        </p>
-
-                            <p>Copyright © 2017, Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.</p>
-                        </div>
+ <div id="application">
+            <div>
+                <div id="wrapper">
+                    <div id="header">
+                        <h1 className="logo"><a href="#/"></a></h1>
+                        <a id="cart-info" href="#">Shopping Cart<span><span data-bind="text: cart.contentsCount"></span> items</span></a>
                     </div>
+                    <p data-bind="visible: cart.cleared" >Thank you for your order!</p>
+                    <div id="main-section">
+                        <section id="pre-content">
+                            <div>
+                                <div id="shop-info" data-bind="attr:{className: cartContentsClass}" className="empty">
 
+                                </div>
+                            </div>
+
+                        </section>
+                        <section id="content">
+                            <div>
+                                <ul data-role="listview" data-bind="source:items" data-template="item" id="main" className="k-widget k-listview" role="listbox"></ul>
+                            </div>
+
+                        </section>
+                        
+                    </div>
+                    <div id="footer">
+                        <p>"Learn more about Kendo UI"
+                        <sup>®</sup>
+                        "SPA at"
+                        <a href="http://docs.telerik.com/kendo-ui/getting-started/framework/spa/overview">docs.telerik.com/kendo-ui</a>
+                        "."
+                        </p>
+                        
+                        <p>Copyright © 2017, Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.</p>
+                    </div>
                 </div>
+
             </div>
-        </body>
+        </div>
 
-    );
-
+       
+      );
+  
 }
 
 export default Body;
