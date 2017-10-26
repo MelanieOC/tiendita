a39f872d177ff2924287068f18071c0bca777720
MelanieOC <melanie.ocharan@gmail.com>
2017-10-26 18:00:18 -0500

    store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import createStore from 'redux-zero';
+import products from './data';
+
+const initialState = {
+    allDish : products
+}
+
+const store = createStore(initialState);
+export default store; 
