d0cc074d4085bd94d5ee479f393c8b5bba00841d
jais210 <jackelineisabel.puruaya@gmail.com>
2017-10-26 15:30:59 -0500

    maquetado

--- src/body.js
+++ src/body.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './body.css';
+import './Body.css';
 
-const Body = ()=> {
-  
-     return (
-        <div id="application">
-            <div>
-                <div id="wrapper">
-                    <div id="header">
-                        <h1 class="logo">
-                        <a href="#/"></a>
-                        </h1>
-                        <a id="cart-info" href="#">
-                         "Shopping Cart"
+const Body = () => {
+
+    return (
+        <body>
+            <div id="application">
+                <div>
+                    <div id="wrapper">
+                        <div id="header">
+                            <h1 class="logo">
+                                <a href="#/"></a>
+                            </h1>
+                            <a id="cart-info" href="#">
+                                "Shopping Cart"
                          <span>
-                             <span data-bing="text: cart.contentsCount"></span>
-                         </span>    
-                        </a> 
-                    </div>
-                    <p data-bind="visible: cart.cleared" style="display:none">Thank you for your order!</p>
-                    <div id="main-section">
-                        <section id="pre-content">
-                            <div>
-                                <div id="shop-info" data-bind="attr:{class: cartContentsClass}" class="empty">
+                                    <span data-bing="text: cart.contentsCount"></span>
+                                </span>
+                            </a>
+                        </div>
+                        <p data-bind="visible: cart.cleared" >Thank you for your order!</p>
+                        <div id="main-section">
+                            <section id="pre-content">
+                                <div>
+                                    <div id="shop-info" data-bind="attr:{class: cartContentsClass}" class="empty">
 
+                                    </div>
                                 </div>
-                            </div>
 
-                        </section>
-                        <section id="content">
-                            <div>
-                                <ul data-role="listview" data-bind="source:items" data-template="item" id="main" class="k-widget k-listview" role="listbox"></ul>
-                            </div>
+                            </section>
+                            <section id="content">
+                                <div>
+                                    <ul data-role="listview" data-bind="source:items" data-template="item" id="main" class="k-widget k-listview" role="listbox"></ul>
+                                </div>
 
-                        </section>
-                        
-                    </div>
-                    <div id="footer">
-                        <p>"Learn more about Kendo UI"
+                            </section>
+
+                        </div>
+                        <div id="footer">
+                            <p>"Learn more about Kendo UI"
                         <sup>®</sup>
-                        "SPA at"
+                                "SPA at"
                         <a href="http://docs.telerik.com/kendo-ui/getting-started/framework/spa/overview">docs.telerik.com/kendo-ui</a>
-                        "."
+                                "."
                         </p>
-                        
-                        <p>Copyright © 2017, Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.</p>
-                    </div>
-                </div>
 
+                            <p>Copyright © 2017, Progress Software Corporation and/or its subsidiaries or affiliates. All Rights Reserved.</p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-        </div>
-      );
-  
+        </body>
+
+    );
+
 }
 
 export default Body;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Body from './Body'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Body />, document.getElementById('root'));
 registerServiceWorker();
