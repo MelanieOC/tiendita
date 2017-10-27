c7e9bfc6e507b5cac8e7d70455a1fbaaab9ed16f
MelanieOC <melanie.ocharan@gmail.com>
2017-10-27 14:03:00 -0500

    que pasa

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import createStore from 'redux-zero';
-import products from './data';
-
-const initialState = {
-    allDish : products
-}
-
-const store = createStore(initialState);
-export default store; 
